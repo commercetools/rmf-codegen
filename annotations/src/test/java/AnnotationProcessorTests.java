1c0211d5158553d64ded862f257875767a95a83a
abeniasaad <achraf.beniasaad@commercetools.de>
2018-07-27 16:17:48 +0200

    Delete unused test

--- annotations/src/test/java/AnnotationProcessorTests.java
+++ annotations/src/test/java/AnnotationProcessorTests.java
@@ -1,29 +1,29 @@
-import com.google.common.base.Joiner;
-import com.google.common.truth.Truth;
-import com.google.testing.compile.JavaFileObjects;
-import com.google.testing.compile.JavaSourcesSubjectFactory;
-import io.vrap.rmf.codegen.common.processor.annotations.ModelExtension;
-import io.vrap.rmf.codegen.common.processor.processor.ModelExtensionProcessor;
-import org.junit.Test;
-
-import javax.tools.JavaFileObject;
-import java.io.File;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-
-public class AnnotationProcessorTests {
-
-
-    @Test
-    public void errorForAnnotatedClass() throws Exception{
-        URL url = Paths.get("/Users/abeniasaad/IdeaProjects/rmf-codegen/annotations/src/main/java/io/vrap/rmf/codegen/common/processor/extension/example/StringExtension.java").toUri().toURL();
-        JavaFileObject INPUT_FILE = JavaFileObjects.forResource(url);
-        Truth.assert_()
-                .about(JavaSourcesSubjectFactory.javaSources())
-                .that(Arrays.asList(INPUT_FILE))
-                .processedWith(new ModelExtensionProcessor())
-                .compilesWithoutError();
-    }
-}
+//import com.google.common.base.Joiner;
+//import com.google.common.truth.Truth;
+//import com.google.testing.compile.JavaFileObjects;
+//import com.google.testing.compile.JavaSourcesSubjectFactory;
+//import io.vrap.rmf.codegen.common.processor.annotations.ModelExtension;
+//import io.vrap.rmf.codegen.common.processor.processor.ModelExtensionProcessor;
+//import org.junit.Test;
+//
+//import javax.tools.JavaFileObject;
+//import java.io.File;
+//import java.net.URL;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.Arrays;
+//
+//public class AnnotationProcessorTests {
+//
+//
+//    @Test
+//    public void errorForAnnotatedClass() throws Exception{
+//        URL url = Paths.get("StringExtension.java").toUri().toURL();
+//        JavaFileObject INPUT_FILE = JavaFileObjects.forResource(url);
+//        Truth.assert_()
+//                .about(JavaSourcesSubjectFactory.javaSources())
+//                .that(Arrays.asList(INPUT_FILE))
+//                .processedWith(new ModelExtensionProcessor())
+//                .compilesWithoutError();
+//    }
+//}
