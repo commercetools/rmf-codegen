fa27180ccff4156ab758fc048c9336672f325271
mkoester <matthias.koester@commercetools.de>
2018-08-06 16:20:44 +0200

    Add generation of property description.

--- common-codegen/src/main/java/io/vrap/rmf/codegen/common/generator/extensions/PropertyExtension.java
+++ common-codegen/src/main/java/io/vrap/rmf/codegen/common/generator/extensions/PropertyExtension.java
@@ -1,7 +1,9 @@
 package io.vrap.rmf.codegen.common.generator.extensions;
 
+import com.google.inject.Inject;
 import io.vrap.rmf.codegen.common.processor.annotations.ExtensionMethod;
 import io.vrap.rmf.codegen.common.processor.annotations.ModelExtension;
+import io.vrap.rmf.raml.model.types.DescriptionFacet;
 import io.vrap.rmf.raml.model.types.Property;
 import io.vrap.rmf.raml.model.types.StringInstance;
 import io.vrap.rmf.raml.model.types.StringType;
@@ -12,10 +14,27 @@ import java.util.stream.Collectors;
 @ModelExtension(extend = Property.class)
 public class PropertyExtension {
 
+    private DescriptionFacetExtension descriptionFacetExtension;
+
+    @Inject
+    public void setDescriptionFacetExtension(final DescriptionFacetExtension descriptionFacetExtension) {
+        this.descriptionFacetExtension = descriptionFacetExtension;
+    }
 
     @ExtensionMethod
     public Boolean  isPatternProperty(Property property){
         return property.getName().startsWith("/");
     }
+
+    @ExtensionMethod
+    public Boolean hasDescription(final Property property) {
+        return property.getType().isInlineType() && descriptionFacetExtension.hasDescription(property.getType());
+    }
+
+    @ExtensionMethod
+    public String getDescription(final Property property) {
+        return descriptionFacetExtension.getDescription(property.getType());
+    }
+
 
 }
