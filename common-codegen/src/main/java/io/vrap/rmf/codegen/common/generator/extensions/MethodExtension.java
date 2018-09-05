429a220aba2da35c3c9c02e7228ed5b14298a2ec
mkoester <matthias.koester@commercetools.de>
2018-09-05 14:25:03 +0200

    Small fix to find the return type. Add spring retry to our generated spring rest client.

--- common-codegen/src/main/java/io/vrap/rmf/codegen/common/generator/extensions/MethodExtension.java
+++ common-codegen/src/main/java/io/vrap/rmf/codegen/common/generator/extensions/MethodExtension.java
@@ -4,6 +4,7 @@ import io.vrap.rmf.codegen.common.processor.annotations.ExtensionMethod;
 import io.vrap.rmf.codegen.common.processor.annotations.ModelExtension;
 import io.vrap.rmf.raml.model.resources.Method;
 import io.vrap.rmf.raml.model.responses.Body;
+import io.vrap.rmf.raml.model.responses.Response;
 import io.vrap.rmf.raml.model.types.AnyType;
 import io.vrap.rmf.raml.model.types.TypedElement;
 import io.vrap.rmf.raml.model.types.impl.TypesFactoryImpl;
@@ -15,7 +16,7 @@ public class MethodExtension {
     @ExtensionMethod
     public AnyType getReturnType(final Method method) {
         return method.getResponses().stream()
-                .filter(r -> r.getStatusCode().equals("200"))
+                .filter(this::isSuccessfulResponse)
                 .filter(response -> (response.getBodies() != null) && (!response.getBodies().isEmpty()))
                 .findFirst()
                 .map(method1 -> method1.getBodies().get(0))
@@ -31,5 +32,14 @@ public class MethodExtension {
     @ExtensionMethod
     public Body getFirstBody(final Method method) {
         return method.getBodies().get(0);
+    }
+
+    private boolean isSuccessfulResponse(final Response response) {
+        try {
+            final int statusCode = Integer.parseInt(response.getStatusCode());
+            return statusCode >= 200 && statusCode < 300;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
