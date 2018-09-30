234d87d3bc169dd4ea3b85d4a47801f724dadc9e
abeniasaad <achraf.beniasaad@commercetools.de>
2018-09-30 18:36:38 +0200

    bug fixing

--- kt-code-generators/src/main/kotlin/io/vrap/codegen/kt/languages/java/JavaBaseTypes.kt
+++ kt-code-generators/src/main/kotlin/io/vrap/codegen/kt/languages/java/JavaBaseTypes.kt
@@ -3,17 +3,28 @@ package io.vrap.codegen.kt.languages.java
 import io.vrap.rmf.codegen.kt.types.LanguageBaseTypes
 import io.vrap.rmf.codegen.kt.types.VrapDefaultObjectType
 import io.vrap.rmf.codegen.kt.types.VrapObjectType
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZonedDateTime
+import kotlin.reflect.KClass
 
 object JavaBaseTypes : LanguageBaseTypes(
 
-        objectType = VrapDefaultObjectType(`package` = "java.lang", simpleClassName = "Object"),
-        integerType = VrapDefaultObjectType(`package` = "java.lang", simpleClassName = "Integer"),
-        doubleType = VrapDefaultObjectType(`package` = "java.lang", simpleClassName = "Double"),
-        stringType = VrapDefaultObjectType(`package` = "java.lang", simpleClassName = "String"),
-        booleanType = VrapDefaultObjectType(`package` = "java.lang", simpleClassName = "Boolean"),
-        dateTimeType = VrapObjectType(`package` = "java.time", simpleClassName = "ZonedDateTime"),
-        dateOnlyType = VrapObjectType(`package` = "java.time", simpleClassName = "TimeOnlyType"),
-        timeOnlyType = VrapObjectType(`package` = "java.time", simpleClassName = "LocalDate")
+        objectType = fromDefaultJavaType(java.lang.Object::class),
+        integerType = fromDefaultJavaType(java.lang.Integer::class),
+        doubleType = fromDefaultJavaType(java.lang.Double::class),
+        stringType = fromDefaultJavaType(java.lang.String::class),
+        booleanType = fromDefaultJavaType(java.lang.Boolean::class),
+        dateTimeType = fromJavaType(ZonedDateTime::class),
+        dateOnlyType = fromJavaType(LocalDate::class),
+        timeOnlyType = fromJavaType(LocalTime::class)
 
 
 )
+
+fun  fromJavaType(kClass: KClass<out Any>):VrapObjectType{
+   return VrapObjectType(kClass.java.`package`.name,kClass.java.simpleName)
+}
+fun  fromDefaultJavaType(kClass: KClass<out Any>):VrapObjectType{
+   return VrapDefaultObjectType(kClass.java.`package`.name,kClass.java.simpleName)
+}
