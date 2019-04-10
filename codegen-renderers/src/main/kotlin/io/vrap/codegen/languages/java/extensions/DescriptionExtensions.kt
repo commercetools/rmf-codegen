2ba7fd2d91266334273a7f8c7a69c045cec83dd5
mkoester <matthias.koester@commercetools.de>
2019-04-10 18:02:39 +0200

    Add support for rendering of "doc" annotation on enum values for java model generator.

--- codegen-renderers/src/main/kotlin/io/vrap/codegen/languages/java/extensions/DescriptionExtensions.kt
+++ codegen-renderers/src/main/kotlin/io/vrap/codegen/languages/java/extensions/DescriptionExtensions.kt
@@ -2,5 +2,31 @@ package io.vrap.codegen.languages.java.extensions
 
 import io.vrap.rmf.codegen.doc.toHtml
 import io.vrap.rmf.raml.model.types.DescriptionFacet
+import io.vrap.rmf.raml.model.types.StringInstance
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
 
-fun DescriptionFacet.toComment() = this.toHtml()?.let {"/**\n${it.lines().map { '\t'+it }.joinToString(separator = "\n")}\n*/"}?:""
+private val HTML_RENDERER = HtmlRenderer.builder().build()
+private val PARSER = Parser.builder().build()
+
+
+fun DescriptionFacet.toComment() = this.toHtml()?.let { "/**\n${it.lines().map { '\t' + it }.joinToString(separator = "\n")}\n*/" }
+    ?: ""
+
+/**
+ * This method extracts the comment from the "doc" annotation - which value needs to be a {@link StringInstance} and
+ * is useful to extract doc for enum values:
+ * <pre>
+ * ResourceType:
+ *    type: string
+ *    enum:
+ *     - value: channel
+ *       (generator.doc): The channel resource type.
+ *     - cart-discount
+ * </pre>
+ */
+fun StringInstance.toComment(): String? {
+    val doc = getAnnotation("doc")?.value?.value
+    return doc?.let { it as String }?.let(PARSER::parse)?.let(HTML_RENDERER::render)?.let { "/**\n${it.lines().map { '\t' + it }.joinToString(separator = "\n")}\n*/" }
+}
+
