c927393843369c58bc12cdaa39c56472e7dd6fa6
abeniasaad <achraf.beniasaad@commercetools.de>
2019-06-20 19:58:27 +0200

    add build files

--- codegen-renderers/src/test/resources/typescript/src/necessary-middlewares/httpClientMiddleware.ts
+++ codegen-renderers/src/test/resources/typescript/src/necessary-middlewares/httpClientMiddleware.ts
@@ -1,37 +1,35 @@
-import {
-  Middleware,
-  MiddlewareArg
-} from "./common-types";
+import { Middleware, MiddlewareArg } from "./common-types";
 import axios = require("axios");
-import fs = require('fs')
+import fs = require("fs");
 
 const client = axios.default;
 
 export const httpMiddleware: Middleware = async (args: MiddlewareArg) => {
-  const { request, error, next } = args;
-
-  if (error != null) {
-    return next(args);
-  }
-
-  var axiosConfig: axios.AxiosRequestConfig;
-  if (request.dataType == "BINARY") {
-    const data = fs.readFileSync(request.payload.filePath)
-    axiosConfig = {
-      method: request.method,
-      url: request.url,
-      data,
-      headers: request.headers
-    };
-  } else {
-    axiosConfig = {
-      method: request.method,
-      url: request.url,
-      data: request.payload,
-      headers: request.headers
-    };
-  }
   try {
+    const { request, error, next } = args;
+
+    if (error != null) {
+      return next(args);
+    }
+
+    var axiosConfig: axios.AxiosRequestConfig;
+    if (request.dataType == "BINARY") {
+      const data = fs.readFileSync(request.payload.filePath);
+      axiosConfig = {
+        method: request.method,
+        url: request.url,
+        data,
+        headers: request.headers
+      };
+    } else {
+      axiosConfig = {
+        method: request.method,
+        url: request.url,
+        data: request.payload,
+        headers: request.headers
+      };
+    }
+
     const response = await client.request(axiosConfig);
     return args.next({
       ...args,
