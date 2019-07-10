52c7f3754de4882125b20dcda496ea06e2fffc11
abeniasaad <achraf.beniasaad@commercetools.de>
2019-07-10 17:27:06 +0200

    fixing resources in the codegenerator

--- typescript_client/dist/gen/models/Error.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-/* tslint:disable */
-//Generated file, please do not change
-Object.defineProperty(exports, "__esModule", { value: true });
--- typescript_client/dist/necessary-middlewares/loggerMiddleware.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loggerMiddleware = async (arg) => {
-    console.log(arg);
-    return arg.next(arg);
-};
