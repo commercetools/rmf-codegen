27c8ba971ddecf83641858c19859c8656f0feadf
alex-ct <aleksandar.petrovic@commercetools.de>
2019-07-24 10:12:58 +0200

    improved request query params generation.

--- languages/javalang/builder-renderer/java-builder-client/src/main/kotlin/io/vrap/codegen/languages/javalang/client/builder/requests/JavaHttpRequestRenderer.kt
+++ languages/javalang/builder-renderer/java-builder-client/src/main/kotlin/io/vrap/codegen/languages/javalang/client/builder/requests/JavaHttpRequestRenderer.kt
@@ -14,11 +14,6 @@ import io.vrap.rmf.codegen.types.VrapTypeProvider
 import io.vrap.rmf.raml.model.resources.Method
 import org.eclipse.emf.ecore.EObject
 
-const val TRAIT_EXPANDABLE : String = "expandable"
-const val TRAIT_QUERY : String = "query"
-const val TRAIT_SORTABLE : String = "sortable"
-const val TRAIT_PAGING : String = "paging"
-
 class JavaHttpRequestRenderer @Inject constructor(override val vrapTypeProvider: VrapTypeProvider) : MethodRenderer, JavaObjectTypeExtensions, EObjectTypeExtensions {
     
     override fun render(type: Method): TemplateFile {
@@ -47,13 +42,16 @@ class JavaHttpRequestRenderer @Inject constructor(override val vrapTypeProvider:
             |   
             |   <${type.executeBlockingMethod()}>
             |   
-            |   <${type.getters()}>
+            |   <${type.pathArgumentsGetters()}>
             |   
-            |   <${type.setters()}>
+            |   <${type.queryParamsGetters()}>
+            |   
+            |   <${type.pathArgumentsSetters()}>
+            |   
+            |   <${type.queryParamsSetters()}>
             |   
             |   <${type.helperMethods()}>
-            |   
-            |   <${type.traits()}>
+            |
             |}
         """.trimMargin().keepIndentation()
         
@@ -89,19 +87,11 @@ class JavaHttpRequestRenderer @Inject constructor(override val vrapTypeProvider:
         val commandClassFields = mutableListOf<String>()
         commandClassFields.add("private HttpHeaders headers = new HttpHeaders();")
         commandClassFields.add("private List<String> additionalQueryParams = new ArrayList<>();")
-        if(this.hasTrait(TRAIT_EXPANDABLE)){
-            commandClassFields.add("private List<String> expansionPath = new ArrayList<>();")
-        }
-        if(this.hasTrait(TRAIT_QUERY)){
-            commandClassFields.add("private List<String> predicate = new ArrayList<>();")
-        }
-        if(this.hasTrait(TRAIT_SORTABLE)){
-            commandClassFields.add("private List<String> sort = new ArrayList<>();")
-        }
-        if(this.hasTrait(TRAIT_PAGING)){
-            commandClassFields.add("private Long offset;")
-            commandClassFields.add("private Long limit;")
-        }
+        val parameterFields : String = 
+                this.queryParameters.map { "private List<${it.type.toVrapType().simpleName()}> ${it.name} = new ArrayList<>();" }
+                        .joinToString(separator = "\n\n")
+        commandClassFields.add(parameterFields)
+        
         this.pathArguments().map { "private String $it;" }.forEach { commandClassFields.add(it) }
         if(this.bodies != null && this.bodies.isNotEmpty()){
             val methodBodyVrapType = this.bodies[0].type.toVrapType() as VrapObjectType
@@ -182,103 +172,33 @@ class JavaHttpRequestRenderer @Inject constructor(override val vrapTypeProvider:
         """.trimMargin().keepIndentation()
     }
     
-    private fun Method.getters() = this.pathArguments()
+    private fun Method.pathArgumentsGetters() : String = this.pathArguments()
             .map { "public String get${it.capitalize()}() {return this.$it;}" }
             .joinToString(separator = "\n")
     
-    private fun Method.setters() = this.pathArguments()
+    private fun Method.pathArgumentsSetters() : String = this.pathArguments()
             .map { "public void set${it.capitalize()}(final String $it) {this.$it = $it;}" }
             .joinToString(separator = "\n\n")
     
-    private fun Method.hasTrait(trait: String) : Boolean {
-        return this.`is`.find { it.trait.name.equals(trait) } != null
-    }
+    private fun Method.queryParamsGetters() : String = this.queryParameters
+            .map { """
+                |public List<${it.type.toVrapType().simpleName()}> get${it.name.capitalize()}() {
+                |   return this.${it.name};
+                |}
+                """.trimMargin().escapeAll() }
+            .joinToString(separator = "\n\n")
     
-    private fun Method.traits() : String {
-        return this.`is`.map { this.trait(it.trait.name) }.joinToString(separator = "\n\n")
-    }
-    
-    private fun Method.trait(trait: String) : String {
-        return when(trait){
-            TRAIT_EXPANDABLE -> {
-                """
-                |public ${this.toRequestName()} addExpansionPath(final String expansionPath) {
-                    |this.expansionPath.add(expansionPath);
-                |   return this;
+    private fun Method.queryParamsSetters() : String = this.queryParameters
+            .map { """
+                |public void add${it.name.capitalize()}(final ${it.type.toVrapType().simpleName()} ${it.name}){
+                |   this.${it.name}.add(${it.name});
                 |}
                 |
-                |public ${this.toRequestName()} withExpansionPath(final List<String> expansionPath) {
-                |   this.expansionPath = expansionPath;
-                |   return this;
+                |public void with${it.name.capitalize()}(final List<${it.type.toVrapType().simpleName()}> ${it.name}){
+                |   this.${it.name} = this.${it.name};
                 |}
-                |
-                |public List<String> getExpansionPath() {
-                |   return this.expansionPath;
-                |}
-                |
-                """.trimMargin().escapeAll()
-            }
-            TRAIT_QUERY -> {
-                """
-                    |public ${this.toRequestName()} addPredicate(final String predicate) {
-                    |   this.predicate.add(predicate);
-                    |   return this;
-                    |}
-                    |
-                    |public ${this.toRequestName()} withPredicate(List<String> predicate) {
-                    |   this.predicate = predicate;
-                    |   return this;
-                    |}
-                    |
-                    |public List<String> getPredicate() {
-                    |   return this.predicate;
-                    |}
-                    |
-                """.trimMargin().escapeAll()
-            }
-            TRAIT_SORTABLE -> {
-                """
-                    |public ${this.toRequestName()} addSort(final String sort) {
-                    |   this.sort.add(sort);
-                    |   return this;
-                    |}
-                    |
-                    |public ${this.toRequestName()} withSort(final List<String> sort) {
-                    |   this.sort = sort;
-                    |   return this;
-                    |}
-                    |
-                    |public List<String> getSort() {
-                    |   return this.sort;
-                    |}
-                    |
-                """.trimMargin().escapeAll()
-            }
-            TRAIT_PAGING -> {
-                """
-                    |public ${this.toRequestName()} withOffset(final Long offset){
-                    |   this.offset = offset;
-                    |   return this;
-                    |}
-                    |
-                    |public ${this.toRequestName()} withLimit(final Long limit){
-                    |   this.limit = limit;
-                    |   return this;
-                    |}
-                    |
-                    |public Long getOffset() {
-                    |   return offset;
-                    |}
-                    |
-                    |public Long getLimit() {
-                    |   return limit;
-                    |}
-                    |
-                """.trimMargin().escapeAll()
-            }
-            else -> ""
-        }
-    }
+            """.trimMargin().escapeAll() }
+            .joinToString(separator = "\n\n")
     
     private fun Method.helperMethods() : String {
         return """
