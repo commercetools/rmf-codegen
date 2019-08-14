3156fc0346ab0cba41f82fbbbb01547822d414b9
alex-ct <aleksandar.petrovic@commercetools.de>
2019-08-14 13:41:39 +0200

    added shopping list integration tests

---
+++ languages/javalang/builder-renderer/generated-code/src/test/java/com/commercetools/shopping_list/ShoppingListIntegrationTests.java
@@ -0,0 +1,116 @@
+package com.commercetools.shopping_list;
+
+import com.commercetools.client.ApiRoot;
+import com.commercetools.models.ShoppingList.*;
+import com.commercetools.utils.CommercetoolsTestUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingListIntegrationTests {
+    
+    @Test
+    public void createAndDeleteById() {
+         ShoppingList shoppingList = ShoppingListFixtures.createShoppingList();
+         ShoppingListFixtures.deleteShoppingList(shoppingList.getId(), shoppingList.getVersion());
+    }
+    
+    @Test
+    public void getById() {
+        ShoppingListFixtures.withShoppingList(shoppingList -> {
+            ShoppingList queriedShoppingList = ApiRoot.withProjectKeyValue(CommercetoolsTestUtils.getProjectKey())
+                    .shoppingLists()
+                    .withId(shoppingList.getId())
+                    .get()
+                    .executeBlocking();
+
+            Assert.assertNotNull(queriedShoppingList);
+            Assert.assertEquals(shoppingList.getId(), queriedShoppingList.getId());
+        });
+    }
+
+    @Test
+    public void getByKey() {
+        ShoppingListFixtures.withShoppingList(shoppingList -> {
+            ShoppingList queriedShoppingList = ApiRoot.withProjectKeyValue(CommercetoolsTestUtils.getProjectKey())
+                    .shoppingLists()
+                    .withKey(shoppingList.getKey())
+                    .get()
+                    .executeBlocking();
+
+            Assert.assertNotNull(queriedShoppingList);
+            Assert.assertEquals(shoppingList.getKey(), queriedShoppingList.getKey());
+        });
+    }
+    
+    @Test
+    public void query() {
+        ShoppingListFixtures.withShoppingList(shoppingList -> {
+            ShoppingListPagedQueryResponse response = ApiRoot.withProjectKeyValue(CommercetoolsTestUtils.getProjectKey())
+                    .shoppingLists()
+                    .get()
+                    .addWhere("id=" + "\"" + shoppingList.getId() + "\"")
+                    .executeBlocking();
+
+            Assert.assertNotNull(response);
+            Assert.assertEquals(response.getResults().get(0).getId(), shoppingList.getId());
+        });
+    }   
+    
+    @Test
+    public void updateByKey() {
+        ShoppingListFixtures.withUpdateableShoppingList(shoppingList -> {
+            
+            List<ShoppingListUpdateAction> updateActions = new ArrayList<>();
+            String newKey = CommercetoolsTestUtils.randomKey();
+            updateActions.add(ShoppingListSetKeyActionBuilder.of().key(newKey).build());
+
+            ShoppingList updatedShoppingList = ApiRoot.withProjectKeyValue(CommercetoolsTestUtils.getProjectKey())
+                    .shoppingLists()
+                    .withKey(shoppingList.getKey())
+                    .post(ShoppingListUpdateBuilder.of()
+                            .version(shoppingList.getVersion())
+                            .actions(updateActions).build())
+                    .executeBlocking();
+
+            Assert.assertNotNull(updatedShoppingList);
+            Assert.assertEquals(updatedShoppingList.getKey(),newKey);
+            
+            return updatedShoppingList;
+        });
+    }
+
+    @Test
+    public void updateById() {
+        ShoppingListFixtures.withUpdateableShoppingList(shoppingList -> {
+
+            List<ShoppingListUpdateAction> updateActions = new ArrayList<>();
+            String newKey = CommercetoolsTestUtils.randomKey();
+            updateActions.add(ShoppingListSetKeyActionBuilder.of().key(newKey).build());
+
+            ShoppingList updatedShoppingList = ApiRoot.withProjectKeyValue(CommercetoolsTestUtils.getProjectKey())
+                    .shoppingLists()
+                    .withId(shoppingList.getId())
+                    .post(ShoppingListUpdateBuilder.of()
+                            .version(shoppingList.getVersion())
+                            .actions(updateActions).build())
+                    .executeBlocking();
+
+            Assert.assertNotNull(updatedShoppingList);
+            Assert.assertEquals(updatedShoppingList.getKey(),newKey);
+
+            return updatedShoppingList;
+        });
+    }
+    
+    @Test
+    public void deleteByKey() {
+        ShoppingList shoppingList = ShoppingListFixtures.createShoppingList();
+        ShoppingList deletedShoppingList = ShoppingListFixtures.deleteShoppingList(shoppingList.getId(), shoppingList.getVersion());
+        Assert.assertNotNull(deletedShoppingList);
+        Assert.assertEquals(shoppingList.getId(), deletedShoppingList.getId());
+    }
+    
+}
