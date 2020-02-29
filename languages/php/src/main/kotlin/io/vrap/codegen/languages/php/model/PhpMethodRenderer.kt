c64ee8b281e377317c93b6304ec00564230d1ad4
Jens Schulze <jenschude@gmail.com>
2020-02-29 22:46:32 +0100

    small change in docblocks

--- languages/php/src/main/kotlin/io/vrap/codegen/languages/php/model/PhpMethodRenderer.kt
+++ languages/php/src/main/kotlin/io/vrap/codegen/languages/php/model/PhpMethodRenderer.kt
@@ -84,7 +84,7 @@ class PhpMethodRenderer @Inject constructor(override val vrapTypeProvider: VrapT
             |    /**
             |     * @template T of JsonObject
             |     * @psalm-param ?class-string<T> $!resultType
-            |     * @psalm-return ${returnTypes.joinToString("|")}|T|null
+            |     * @return ${returnTypes.joinToString("|")}|T|null
             |     */
             |    public function mapFromResponse(?ResponseInterface $!response, string $!resultType = null)
             |    {
