3701cbc728d52562fed9018e23db43905ae23435
Matthias Köster <katmatt@users.noreply.github.com>
2020-04-27 10:30:34 +0200

    Fix vrap type. (#154)

--- languages/typescript/src/main/kotlin/io/vrap/codegen/languages/typescript/server/ServerRenderer.kt
+++ languages/typescript/src/main/kotlin/io/vrap/codegen/languages/typescript/server/ServerRenderer.kt
@@ -230,7 +230,7 @@ class ServerRenderer @Inject constructor(
         return when (this) {
             is VrapObjectType -> {
                 val joiType = this.toJoiVrapType()
-                "import { ${joiType.simpleClassName} } from '@joi/${joiType.`package`}'"
+                "import { ${joiType.simpleClassName} } from '${joiType.`package`}'"
             }
             is VrapArrayType -> {
                 return this.itemType.joiImportStatement()
