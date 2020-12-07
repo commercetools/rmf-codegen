1ff85885492608e6887dee46eb366ac138befe1f
Nikolaus Kühn <nikolaus.kuehn@commercetools.de>
2020-12-07 16:20:59 +0100

    update graalvm version

--- tools/cli-application/generate-graal-config.sh
+++ tools/cli-application/generate-graal-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir -p META-INF/native-image
 ../../gradlew build
-~/.gradle/caches/com.palantir.graal/20.2.0/11/graalvm-ce-java11-20.2.0/Contents/Home/bin/java -agentlib:native-image-agent=config-output-dir=./META-INF/native-image -jar ../../rmf-gen.jar generate -o ./build/tmp -t RAML_DOC ../../api-spec/api.raml
+~/.gradle/caches/com.palantir.graal/20.3.0/11/graalvm-ce-java11-20.3.0/Contents/Home/bin/java -agentlib:native-image-agent=config-output-dir=./META-INF/native-image -jar ../../rmf-gen.jar generate -o ./build/tmp -t RAML_DOC ../../api-spec/api.raml
