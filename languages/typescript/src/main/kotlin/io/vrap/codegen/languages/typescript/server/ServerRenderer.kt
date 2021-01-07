ba67c6b09bffa8072a37dc6aba4c0b220b58e430
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2021-01-07 11:03:58 +0100

    update hapi params to use joi objects

--- languages/typescript/src/main/kotlin/io/vrap/codegen/languages/typescript/server/ServerRenderer.kt
+++ languages/typescript/src/main/kotlin/io/vrap/codegen/languages/typescript/server/ServerRenderer.kt
@@ -136,9 +136,9 @@ class ServerRenderer @Inject constructor(
                         |   options: {
                         |      validate: {
                         |        <${if(it.hasBody()) "payload: ${it.bodies[0].type.toVrapType().simpleJoiName()}()," else ""}>
-                        |        params: {
+                        |        params: Joi.object({
                         |          <${it.resource().fullUri.variables.map { "$it: requiredString" }.joinToString(separator = ",\n")}>
-                        |        },
+                        |        }),
                         |        options: {
                         |          abortEarly: false
                         |        },
