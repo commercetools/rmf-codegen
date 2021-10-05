072ea23bd6a5ab49968899345842a17a6bf54b7f
Jens Schulze <jens.schulze@commercetools.com>
2021-10-05 14:29:05 +0200

    Update AsMapRule.kt

--- ctp-validators/src/main/kotlin/com/commercetools/rmf/validators/AsMapRule.kt
+++ ctp-validators/src/main/kotlin/com/commercetools/rmf/validators/AsMapRule.kt
@@ -2,7 +2,6 @@ package com.commercetools.rmf.validators
 
 import io.vrap.rmf.raml.model.types.ObjectType
 import io.vrap.rmf.raml.model.types.Property
-//import io.vrap.rmf.raml.model.util.StringCaseFormat
 import org.eclipse.emf.common.util.Diagnostic
 import java.util.*
 
@@ -14,7 +13,6 @@ class AsMapRule(options: List<RuleOption>? = null) : TypesRule(options) {
     override fun caseObjectType(type: ObjectType): List<Diagnostic> {
         val validationResults: MutableList<Diagnostic> = ArrayList()
 
-        // write the property rule validation here...
         if (exclude.contains(type.name).not()) {
             if (type.properties.size == 1 && type.properties[0].isPatternProperty()) {
                 if (type.getAnnotation("asMap") == null) {
