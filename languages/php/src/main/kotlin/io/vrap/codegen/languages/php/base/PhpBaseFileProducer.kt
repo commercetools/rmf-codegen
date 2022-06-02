609b1bdf280f5887e853854fee0a26b757b93cfd
Jens Schulze <jens.schulze@commercetools.com>
2022-06-02 21:46:03 +0200

    extract php cache validation

--- languages/php/src/main/kotlin/io/vrap/codegen/languages/php/base/PhpBaseFileProducer.kt
+++ languages/php/src/main/kotlin/io/vrap/codegen/languages/php/base/PhpBaseFileProducer.kt
@@ -23,6 +23,7 @@ class PhpBaseFileProducer constructor(val api: Api, @BasePackageName val package
             baseNullable(),
             builder(),
             cachedProvider(),
+            cacheValidator(),
             clientCredentials(),
             clientCredentialsConfig(),
             clientFactory(),
@@ -824,42 +825,24 @@ class PhpBaseFileProducer constructor(val api: Api, @BasePackageName val package
                     |{
                     |    /**
                     |     * @psalm-param CacheItemPoolInterface|CacheInterface|null $!cache
-                    |     * @psalm-return CacheItemPoolInterface|CacheInterface
-                    |     */
-                    |    private static function validateCache($!cache = null)
-                    |    {
-                    |        if ($!cache instanceof CacheItemPoolInterface || $!cache instanceof CacheInterface) {
-                    |            return $!cache;
-                    |        }
-                    |
-                    |        $!filesystemAdapter = new Local(getcwd());
-                    |        $!filesystem        = new Filesystem($!filesystemAdapter);
-                    |        $!cache = new FilesystemCachePool($!filesystem);
-                    |        
-                    |        return $!cache;
-                    |    }
-                    |
-                    |    /**
-                    |     * @psalm-param CacheItemPoolInterface|CacheInterface|null $!cache
                     |     */
                     |    public static function ofAuthConfig(AuthConfig $!authConfig, $!cache = null): OAuth2Handler
                     |    {
-                    |        $!cache = self::validateCache($!cache);
+                    |        $!cache = CacheValidator::validateCache($!cache);
                     |        switch(true) {
-                    |           case $!authConfig instanceof ClientCredentialsConfig:
-                    |               $!provider = new CachedTokenProvider(
-                    |                   new ClientCredentialTokenProvider(
-                    |                       new Client($!authConfig->getOptions()),
-                    |                       $!authConfig->getAuthUri(),
-                    |                       $!authConfig->getCredentials()
-                    |                   ),
-                    |                   $!cache,
-                    |                   $!authConfig->getCredentials()->getCacheKey()
-                    |               );
-                    |               break;
-                    |           default:
-                    |               throw new InvalidArgumentException('Unknown authorization configuration');
-                    |
+                    |            case $!authConfig instanceof ClientCredentialsConfig:
+                    |                $!provider = new CachedTokenProvider(
+                    |                    new ClientCredentialTokenProvider(
+                    |                        new Client($!authConfig->getOptions()),
+                    |                        $!authConfig->getAuthUri(),
+                    |                        $!authConfig->getCredentials()
+                    |                    ),
+                    |                    $!cache,
+                    |                    $!authConfig->getCredentials()->getCacheKey()
+                    |                );
+                    |                break;
+                    |            default:
+                    |                throw new InvalidArgumentException('Unknown authorization configuration');
                     |        }
                     |        return self::ofProvider($!provider);
                     |    }
@@ -2825,6 +2808,45 @@ class PhpBaseFileProducer constructor(val api: Api, @BasePackageName val package
                 """.trimMargin().forcedLiteralEscape())
     }
 
+    private fun cacheValidator(): TemplateFile {
+        return TemplateFile(relativePath = "src/Client/CacheValidator.php",
+            content = """
+                    |<?php
+                    |${PhpSubTemplates.generatorInfo}
+                    |
+                    |namespace ${packagePrefix.toNamespaceName()}\Client;
+                    |
+                    |use Psr\Cache\CacheItemPoolInterface;
+                    |use Psr\SimpleCache\CacheInterface;
+                    |use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+                    |
+                    |final class CacheValidator
+                    |{
+                    |    /**
+                    |     * @psalm-param CacheItemPoolInterface|CacheInterface|null $!cache
+                    |     * @psalm-return CacheItemPoolInterface|CacheInterface
+                    |     */
+                    |    public static function validateCache($!cache = null)
+                    |    {
+                    |        if ($!cache instanceof CacheItemPoolInterface || $!cache instanceof CacheInterface) {
+                    |            return $!cache;
+                    |        }
+                    |
+                    |        if (class_exists('Symfony\Component\Cache\Simple\FilesystemCache')) {
+                    |            /** @psalm-suppress all */
+                    |            /** @var CacheItemPoolInterface $!cache */
+                    |            $!cache = new FilesystemCache('', 0, getcwd() . "/cache");
+                    |        } else {
+                    |            /** @psalm-suppress all */
+                    |            /** @var CacheItemPoolInterface $!cache */
+                    |            $!cache = new FilesystemAdapter('', 0, getcwd() . "/cache");
+                    |        }
+                    |
+                    |        return $!cache;
+                    |    }
+                    |}
+                """.trimMargin().forcedLiteralEscape())
+    }
     private fun cachedProvider(): TemplateFile {
         return TemplateFile(relativePath = "src/Client/CachedTokenProvider.php",
                 content = """
