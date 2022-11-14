82040ce6038815a079cac976a4849862d9cc5e61
Jens Schulze <jens.schulze@commercetools.com>
2022-11-14 21:35:15 +0100

    add diff check tests

--- tools/cli-application/src/test/groovy/io/vrap/rmf/codegen/cli/diff/DiffCheckTest.groovy
+++ tools/cli-application/src/test/groovy/io/vrap/rmf/codegen/cli/diff/DiffCheckTest.groovy
@@ -63,6 +63,85 @@ class DiffCheckTest extends Specification implements ValidatorFixtures {
         results[1].message == "removed method `get /bar`"
     }
 
+    def "added property"() {
+        when:
+        def check = diff("/property-added", new PropertyAddedCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 1
+        results[0].message == "added property `bar` to type `Foo`"
+    }
+
+    def "removed property"() {
+        when:
+        def check = diff("/property-removed", new PropertyRemovedCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 1
+        results[0].message == "removed property `bar` from type `Foo`"
+    }
+
+    def "added query parameter"() {
+        when:
+        def check = diff("/query-parameter-added", new QueryParameterAddedCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 1
+        results[0].message == "added query parameter `bar` to method `get /foo`"
+    }
+
+    def "removed query parameter"() {
+        when:
+        def check = diff("/query-parameter-removed", new QueryParameterRemovedCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 1
+        results[0].message == "removed query parameter `bar` from method `get /foo`"
+    }
+
+    def "property type changed"() {
+        when:
+        def check = diff("/property-change-type", new PropertyTypeChangedCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 1
+        results[0].message == "changed property `bar` of type `Foo` from type `string` to `object`"
+    }
+
+    def "property optional"() {
+        when:
+        def check = diff("/property-optional", new PropertyOptionalCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 4
+        results[0].message == "changed property `foo` of type `Foo` to be optional"
+        results[1].message == "changed property `baz` of type `Baz` to be optional"
+        results[2].message == "changed property `fooBaz` of type `FooBaz` to be optional"
+        results[3].message == "changed property `fooBarBaz` of type `FooBarBaz` to be optional"
+    }
+
+    def "property required"() {
+        when:
+        def check = diff("/property-required", new PropertyRequiredCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 4
+        results[0].message == "changed property `foo` of type `Foo` to be required"
+        results[1].message == "changed property `baz` of type `Baz` to be required"
+        results[2].message == "changed property `fooBaz` of type `FooBaz` to be required"
+        results[3].message == "changed property `fooBarBaz` of type `FooBarBaz` to be required"
+    }
+
+    def "type change"() {
+        when:
+        def check = diff("/type-change", new TypeChangedCheck(defaultSeverity))
+        def results = check.diff()
+        then:
+        results.size() == 2
+        results[0].message == "changed type `Foo` from type `object` to `string`"
+        results[1].message == "changed type `Bar` from type `Foo` to `Baz`"
+    }
+
     private <T> RamlDiff diff(String fileLocation, Differ<T> check) {
         return new RamlDiff.Builder()
                     .original(readApi(fileLocation + "-original.raml"))
