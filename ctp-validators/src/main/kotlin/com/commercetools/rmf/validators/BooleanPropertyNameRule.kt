23c9bada8175b471531c36d052e6071e1e143d69
Jens Schulze <jens.schulze@commercetools.com>
2024-01-15 16:59:09 +0100

    Update BooleanPropertyNameRule.kt

--- ctp-validators/src/main/kotlin/com/commercetools/rmf/validators/BooleanPropertyNameRule.kt
+++ ctp-validators/src/main/kotlin/com/commercetools/rmf/validators/BooleanPropertyNameRule.kt
@@ -4,7 +4,7 @@ import io.vrap.rmf.raml.model.types.*
 import org.eclipse.emf.common.util.Diagnostic
 import java.util.*
 
-@ValidatorSet
+//@ValidatorSet
 class BooleanPropertyNameRule(severity: RuleSeverity, options: List<RuleOption>? = null) : TypesRule(severity, options) {
 
     private val exclude: List<String> =
