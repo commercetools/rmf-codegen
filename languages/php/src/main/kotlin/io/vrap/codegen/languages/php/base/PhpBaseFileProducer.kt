78dd4039bd1adf1529005a97720f2a10df56bea8
Barbara Palumbo <barbara.palumbo@commercetools.de>
2024-07-04 10:35:40 +0200

    reviewed a misspelling

--- languages/php/src/main/kotlin/io/vrap/codegen/languages/php/base/PhpBaseFileProducer.kt
+++ languages/php/src/main/kotlin/io/vrap/codegen/languages/php/base/PhpBaseFileProducer.kt
@@ -2760,7 +2760,8 @@ class PhpBaseFileProducer constructor(val api: Api, @BasePackageName val package
                     |     * @param string $!clientSecret
                     |     *   The client secret.
                     |     * @param string $!scope
-                    |     *   Provide the scope when you want to request a specific ones for the client. Can be ommitted to use all scopes of the oauth client.
+                    |     *   Provide the scope when you want to request a specific ones for the client. 
+                    |     *   Can be omitted to use all scopes of the oauth client.
                     |     *   Format: `<the scope name>:<the project key>`.
                     |     *   Example: `manage_products:project1`.
                     |     */
